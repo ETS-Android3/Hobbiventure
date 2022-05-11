89568fc4e021299ec5b7e86e5497a47585c5f2b9
Doffyloffy <jonas.toftgaard@hotmail.com>
2022-05-11 17:05:20 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/bottomnavigation/ui/survey/SurveyViewModel.java
@@ -0,0 +1,19 @@
+package com.example.bottomnavigation.ui.survey;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SurveyViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SurveyViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is survey fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
