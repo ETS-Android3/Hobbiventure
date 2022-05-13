dbba88fef0cf25c763629a114ef8d8f16a73b37d
Charliecoolswag <charliecoolswag@gmail.com>
2022-05-13 11:46:38 +0200

    Test om det virker

--- app/src/main/java/com/example/bottomnavigation/ui/profile/ProfileActivity.java
+++ app/src/main/java/com/example/bottomnavigation/ui/profile/ProfileActivity.java
@@ -1,7 +1,12 @@
 package com.example.bottomnavigation.ui.profile;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -11,7 +16,18 @@ import com.example.bottomnavigation.R;
 import com.example.bottomnavigation.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class ProfileActivity {
+public class ProfileActivity extends AppCompatActivity {
+    private Button button;
 
+    @SuppressLint("ResourceType")
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_profile);
+        button.findViewById(R.id.button3);
+        button.setOnClickListener(view ->{
+            startActivity(new Intent(this,SettingsActivity.class));
+        });
+    }
 
 }
