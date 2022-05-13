8c0ca1c6141eaceec3270a39f9f0a3e57823d8e3
chrizzols <olsen.christian15@gmail.com>
2022-05-13 13:34:57 +0200

    Profile fragment work

--- app/src/main/java/com/example/bottomnavigation/ui/profile/ProfileActivity.java
+++ app/src/main/java/com/example/bottomnavigation/ui/profile/ProfileActivity.java
@@ -1,33 +1,73 @@
 package com.example.bottomnavigation.ui.profile;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.Button;
+import static android.content.ContentValues.TAG;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.bottomnavigation.R;
-import com.example.bottomnavigation.databinding.ActivityMainBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    String email = user.getEmail();
     private Button button;
+    private TextView textView;
+    private EditText editText;
 
     @SuppressLint("ResourceType")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_profile);
-        button.findViewById(R.id.button3);
-        button.setOnClickListener(view ->{
-            startActivity(new Intent(this,SettingsActivity.class));
-        });
+
+        textView = textView.findViewById(R.id.tvCurrEmail);
+        textView.setText(email);
+
+
+
+    }
+
+    public void updateEmail(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        editText = editText.findViewById(R.id.etUpdEmail);
+        user.updateEmail(editText.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Email successfully updated!");
+                        }
+                    }
+                });
+
+    }
+
+    public void updatePassword(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        editText = editText.findViewById(R.id.etUpdPass);
+        user.updatePassword(editText.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Password successfully updated!");
+                        }
+                    }
+                });
+
     }
 
 }
--- app/src/main/java/com/example/bottomnavigation/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/bottomnavigation/ui/profile/ProfileFragment.java
@@ -1,23 +1,29 @@
 package com.example.bottomnavigation.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.bottomnavigation.R;
 import com.example.bottomnavigation.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
     TextView textView;
+    private Button button;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
 
         ProfileViewModel profileViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
 
@@ -27,6 +33,7 @@ public class ProfileFragment extends Fragment {
         //final TextView textView = binding.textProfile;
         //profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+
     }
 
     @Override
