030c42bd236cd269e5903eaa50b07cbd7ef3f0f6
Doffyloffy <jonas.toftgaard@hotmail.com>
2022-05-15 14:24:15 +0200

    Tilføjet billeder til alle places i kategorierne og hvad der hører til

--- app/src/main/java/com/example/bottomnavigation/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/bottomnavigation/ui/search/SearchFragment.java
@@ -12,18 +12,16 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.bottomnavigation.R;
 import com.example.bottomnavigation.databinding.FragmentSearchBinding;
 
-import java.util.ArrayList;
 
 public class SearchFragment extends Fragment /*implements View.OnClickListener*/ {
 
     private FragmentSearchBinding binding;
-    private CategoryViewModel categoryViewModel;
-    private CategoryFragment categoryFragment;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -32,22 +30,24 @@ public class SearchFragment extends Fragment /*implements View.OnClickListener*/
 
         binding = FragmentSearchBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        final ImageButton imageButton1 = binding.category1;
-        final ImageButton imageButton2 = binding.category2;
-        final ImageButton imageButton3 = binding.category3;
-        final ImageButton imageButton4 = binding.category4;
+
+        final ImageButton imageButton1 = binding.udendoerssport;
+        final ImageButton imageButton2 = binding.vildmark;
+        final ImageButton imageButton3 = binding.indendoerssport;
+        final ImageButton imageButton4 = binding.hjemkundskab;
         final TextView outdoor = binding.textSearchOutdoor;
         final TextView indoor = binding.textSearchIndoor;
         final TextView events = binding.textSearchEvents;
         searchViewModel.getTextOutdoor().observe(getViewLifecycleOwner(), outdoor::setText);
         searchViewModel.getTextIndoor().observe(getViewLifecycleOwner(), indoor::setText);
         searchViewModel.getTextEvents().observe(getViewLifecycleOwner(), events::setText);
-
         final FragmentTransaction fr = getParentFragmentManager().beginTransaction();
         imageButton1.setOnClickListener(view -> {
             fr.replace(R.id.nav_host_fragment_activity_main,new CategoryFragment());
             fr.commit();
+
         });
+
         /*imageButton2.setOnClickListener(this);
         imageButton3.setOnClickListener(this);
         imageButton4.setOnClickListener(this);*/
@@ -63,8 +63,6 @@ public class SearchFragment extends Fragment /*implements View.OnClickListener*/
     public void onClick(final View view) {
 switch (view.getId()){
     case R.id.category1:
-    fr.replace(R.id.nav_host_fragment_activity_main, new CategoryFragment());
-    fr.commit();
     break;
    /* case R.id.category2:
         break;
