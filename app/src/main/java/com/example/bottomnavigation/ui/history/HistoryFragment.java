75e99ee7b3bd51ec3a373a7c5deb2d4ad1567fe1
Charliecoolswag <charliecoolswag@gmail.com>
2022-05-16 16:14:42 +0200

    API med google maps virker nu. Mangler decideret lokation den skal pege på

--- app/src/main/java/com/example/bottomnavigation/ui/history/HistoryFragment.java
+++ app/src/main/java/com/example/bottomnavigation/ui/history/HistoryFragment.java
@@ -4,29 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.bottomnavigation.R;
 import com.example.bottomnavigation.databinding.FragmentHistoryBinding;
+import com.example.bottomnavigation.ui.home.MapsFragment;
 
 public class HistoryFragment extends Fragment {
 
     private FragmentHistoryBinding binding;
+    private HistoryViewModel historyViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HistoryViewModel historyViewModel =
-                new ViewModelProvider(this).get(HistoryViewModel.class);
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        HistoryViewModel historyViewModel = new ViewModelProvider(this).get(HistoryViewModel.class);
         binding = FragmentHistoryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        final Button buttonB = binding.buttonMaps;
         final TextView textView = binding.textHistory;
-        historyViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-
+        final FragmentTransaction fr = getParentFragmentManager().beginTransaction();
+        buttonB.setOnClickListener(view -> {
+            fr.replace(R.id.nav_host_fragment_activity_main, new MapsFragment());
+            fr.commit();
+        });
 
         return root;
     }
---
+++ app/src/main/java/com/example/bottomnavigation/ui/home/MapsFragment.java
@@ -0,0 +1,83 @@
+package com.example.bottomnavigation.ui.home;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bottomnavigation.R;
+import com.example.bottomnavigation.databinding.ActivityMapsBinding;
+import com.example.bottomnavigation.databinding.FragmentHistoryBinding;
+import com.example.bottomnavigation.ui.history.HistoryViewModel;
+import com.example.bottomnavigation.ui.search.CategoryFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MapsFragment extends Fragment {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        HistoryViewModel historyViewModel = new ViewModelProvider(this).get(HistoryViewModel.class);
+        View view = inflater.inflate(R.layout.activity_maps, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady( GoogleMap googleMap) {
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(LatLng latLng) {
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        markerOptions.position(latLng);
+
+                        markerOptions.title(latLng.latitude + "56.20504565808364, 10.18151342350171" + latLng.longitude);
+                        googleMap.clear();
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                        googleMap.addMarker(markerOptions);
+                    }
+                });
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        final FragmentTransaction fr = getParentFragmentManager().beginTransaction();
+        fr.detach(MapsFragment.this);
+        fr.commit();
+        binding = null;
+    }
+}
+
+/**
+ * Manipulates the map once available.
+ * This callback is triggered when the map is ready to be used.
+ * This is where we can add markers or lines, add listeners or move the camera. In this case,
+ * we just add a marker near Sydney, Australia.
+ * If Google Play services is not installed on the device, the user will be prompted to install
+ * it inside the SupportMapFragment. This method will only be triggered once the user has
+ * installed Google Play services and returned to the app.
+ *
+ * @Override public void onMapReady(GoogleMap googleMap) {
+ * mMap = googleMap;
+ * <p>
+ * // Add a marker in Sydney and move the camera
+ * LatLng BouldersAarhus = new LatLng(56.20504565808364, 10.18151342350171);
+ * mMap.addMarker(new MarkerOptions().position(BouldersAarhus).title("Marker on Aarhus Boulders"));
+ * mMap.moveCamera(CameraUpdateFactory.newLatLng(BouldersAarhus));
+ * }
+ */
