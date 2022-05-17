cd788a23b2ae76e830573683a2b34007c7f1baf3
azimndiaye <294997@via.dk>
2022-05-17 09:57:56 +0200

    History

--- app/src/main/java/com/example/bottomnavigation/ui/history/HistoryFragment.java
+++ app/src/main/java/com/example/bottomnavigation/ui/history/HistoryFragment.java
@@ -8,13 +8,21 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.bottomnavigation.databinding.FragmentHistoryBinding;
+import com.example.bottomnavigation.ui.survey.Place;
+import com.example.bottomnavigation.ui.survey.ResultFragment;
+
+import java.util.ArrayList;
 
 public class HistoryFragment extends Fragment {
 
+
     private FragmentHistoryBinding binding;
+    HistoryAdapter historyAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,8 +33,11 @@ public class HistoryFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView textView = binding.textHistory;
-        historyViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-
+        final RecyclerView recyclerView = binding.HistoryRec;
+        ArrayList<History> histories = new ArrayList<>();
+        histories.add(new History("Asa Fodboldklub - Fodbold"));
+        historyAdapter = new HistoryAdapter(histories);
+        recyclerView.setAdapter(historyAdapter);
 
         return root;
     }
@@ -34,6 +45,9 @@ public class HistoryFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        final FragmentTransaction fr = getParentFragmentManager().beginTransaction();
+        fr.detach(HistoryFragment.this);
+        fr.commit();
         binding = null;
     }
 }
